0938541b01fe5dd2fdb842cc8e6df8c55f237002
Muhamad Biki Hamzah <39186685+bicky33@users.noreply.github.com>
2024-04-25 08:34:18 +0700

    remove process env

--- src/app/services/albums/albums.service.ts
+++ src/app/services/albums/albums.service.ts
@@ -12,7 +12,7 @@ export class AlbumsService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  urlApi: string = `${process.env['BaseURL']}/albums` ?? `${environment.baseUrl}/albums`
+  urlApi: string = `${environment.baseUrl}/albums`
   constructor(private http:HttpClient) { }
 
   getAlbumsByUserId(userId: string): Observable<Album[]> {
--- src/app/services/comments/comments.service.ts
+++ src/app/services/comments/comments.service.ts
@@ -14,7 +14,7 @@ export class CommentsService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  urlApi: string = `${process.env['BaseURL']}/comments` ?? `${environment.baseUrl}/comments`
+  urlApi: string = `${environment.baseUrl}/comments`
 
   getCommentsByPostId(postId: number): Observable<Comment[]> {
     return this.http.get<Comment[]>(`${this.urlApi}?postId=${postId}`);
